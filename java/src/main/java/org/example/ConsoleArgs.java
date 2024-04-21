5dd8ced8825610fc88205832cafa6c77f446833c
GameTV1 <gametvcity.com@gmail.com>
2024-04-21 23:46:21 +0200

    Add java

---
+++ java/src/main/java/org/example/ConsoleArgs.java
@@ -0,0 +1,79 @@
+package org.example;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class ConsoleArgs {
+    static boolean isNumber(String strNum) {
+        try {
+            Integer.parseInt(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    static List<Integer> processNumbers(List<Integer> numbers) {
+        if (numbers.size() % 2 == 0) {
+            return numbers.stream().filter(num -> num % 2 == 0).collect(Collectors.toList());
+        }
+        return numbers.stream().filter(num -> num % 2 != 0).collect(Collectors.toList());
+    }
+
+    static void outputResult(List<Integer> result, String outputFile) {
+        if (outputFile != null) {
+            if (!outputFile.endsWith(".txt")) {
+                outputFile += ".txt";
+            }
+            try {
+                Files.write(Paths.get(outputFile), (Iterable<String>)result.stream().map(Object::toString)::iterator);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println(result.stream().map(Object::toString).collect(Collectors.joining(" ")));
+        }
+    }
+
+    static void readFromStdin(String outputFile) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter each number: ");
+        String numbersString = scanner.nextLine();
+        List<Integer> numbers = Arrays.stream(numbersString.trim().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        List<Integer> result = processNumbers(numbers);
+        outputResult(result, outputFile);
+        System.out.print("Click Enter for finishing... ");
+        scanner.nextLine();
+        scanner.close();
+    }
+
+    static void readFromFile(String inputFile, String outputFile) {
+        try {
+            String content = new String(Files.readAllBytes(Paths.get(inputFile)));
+            List<Integer> numbers = Arrays.stream(content.split("\\s+"))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            List<Integer> result = processNumbers(numbers);
+            outputResult(result, outputFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        String input = args.length > 0 ? args[0] : null;
+        String output = args.length > 1 ? args[1] : null;
+
+        if (input == null || isNumber(input)) {
+            readFromStdin(output);
+        } else {
+            readFromFile(input, output);
+        }
+    }
+}
--- js/READme.md
+++ js/READme.md
@@ -24,19 +24,18 @@ Aplikace přečte čísla ze souboru, výsledek zapíše na s. výstup:
 aplikace input.txt
 
 ### Návod
-Java máme v /java. Jar soubory jsou v /jarFiles.
-- exeArgs.bat - soubor, ve kterém napíšete parametry po spuštění, máte spustit "exeArgs.bat" soubor. `Enter args: ` přes mezerník (space) 1 nebo 2 argumenty: způsob vstupu a soubor vystupu (nebo prazdné - konzole). Pokud budete psát v konzoli, napíšete pole čísel, kliknutím `Enter` skončíte vstup
-- consoleArgs.jar - soubor, pro který píšete parametry a spustítě přes Windows Power Shell, napíšete `java -jar .\consoleArgs.jar [par1] [par2]`.
-- Ještě můžete spustit aplikaci v standardním .java souboru
+- exeArgs.exe - soubor, ve kterém píšete parametry ve vnitřní kozoli aplikaci `Enter args: ` přes mezerník (space) 1 nebo 2 argumenty: způsob vstupu a soubor vystupu (nebo prazdné - konzole). Pokud budete psát v konzoli, napíšete pole čísel, kliknutím `Enter` skončíte vstup
+- consoleArgs.exe - soubor, ve kterém píšete parametry a spustítě přes Windows Power Shell, např. Napíšete `.\consoleArgs.exe (1 parameter) (2 parameter)`. Pokud spustíte 2x kliknutím exe, bude to považovano za konzolní vstup a výstup.
+- Ještě můžete spustit aplikaci přes `node *.js (1 parameter) (2 parameter)`
 
-### Příklady vstupu pro exeArgs.bat
+### Příklady vstupu pro exeArgs.exe
 - `5` - Konzole pro vstup a výstup
 - `input.txt out` Soubor pro vstup a výstup. Soubor výstupu můžete napsát s .txt nebo bez, automaticky to bude překládano do .txt
 - `input.txt` Souborový vstup, konzolní výstup
 - `10 output.txt` Konzolní vstup, souborový výstup
 
-### Příklady vstupu pro consoleArgs.jar (v PowerShell)
-- `java -jar .\consoleArgs.jar input.txt output` - Souborový vstup a výstup
-- `java -jar .\consoleArgs.jar input.txt` Soubor pro vstup, konzole pro výstup.
-- `java -jar .\consoleArgs.jar 5` Konzolní vstup a výstup
-- `java -jar .\consoleArgs.jar 5 output` Konzolní vstup, souborový výstup
+### Příklady vstupu pro consoleArgs.exe (v PowerShell)
+- `.\consoleArgs.exe input.txt output` - Souborový vstup a výstup
+- `.\consoleArgs.exe input.txt` Soubor pro vstup, konzole pro výstup.
+- `.\consoleArgs.exe 5` Konzolní vstup a výstup
+- `.\consoleArgs.exe 5 output` Konzolní vstup, souborový výstup
